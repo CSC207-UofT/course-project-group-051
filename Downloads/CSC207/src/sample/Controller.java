95586d17365d88b409eb6b31f737e35cf30b2571
guanfei1 <vi.guan@Mail.utoronto.ca>
2021-10-15 02:12:29 -0400

    i

---
+++ Downloads/CSC207/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.awt.image.BufferedImage;
+import java.util.Date;
+import java.util.List;
+
+public class Controller {
+public Controller(){
+}
+public void createNewUser(Database database, String firstName, String lastName, Date DOB, String Occupation, BufferedImage profilePic,
+                          List<String> hobbies, String bio, String gender, String orientation, String userID, String pw) {
+    User u = new User(firstName, lastName, DOB, Occupation, profilePic, hobbies, bio, gender, orientation, userID);
+    if (!database.contains(u)) {
+        database.add(u);
+
+    } else {
+        System.out.println("You already have an account with us. Please log in");
+    }
+}
+
+}
+

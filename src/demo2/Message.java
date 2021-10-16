d452e23309ea1fed7bb08113406c7729ad014505
ShengHe Xiao <benny.xiao@mail.utoronto.ca>
2021-10-15 23:32:49 -0400

    changes message

--- src/demo2/Message.java
+++ src/demo2/Message.java
@@ -1,4 +1,24 @@
 package demo2;
 
 public class Message {
+    private User sender;
+    private User receiver;
+    private String msg;
+    public Message(User sender, User receiver, String msg){
+        this.sender = sender;
+        this.receiver = receiver;
+        this.msg = msg;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
 }

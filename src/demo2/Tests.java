31b2c2982636ec1e98f271432227876c002f565e
vFf0621 <victorkwan1206@yahoo.com>
2021-10-16 04:01:49 -0400

    Adde

---
+++ src/demo2/Tests.java
@@ -0,0 +1,31 @@
+
+package demo2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class Tests {
+    @Test
+    void TestDataBase1(){
+        User u = new User(1, "foo", "foo");
+        Database d = new Database();
+        d.addUser(u);
+        ArrayList a = new ArrayList();
+        a.add(u);
+        assert(d.getUsers().equals(a));
+
+    }
+
+    @Test
+    void TestUser1(){
+        User u = new User(1, "foo", "foo");
+        u.setAge(2);
+        u.setGender("F");
+        u.setUsername("1");
+        assert(u.getAge() == 2);
+        assert(u.getGender() == "F");
+        assert(u.getUsername() == "1");
+    }
+
+}

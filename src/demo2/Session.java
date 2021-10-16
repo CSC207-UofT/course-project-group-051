98a454e95e7920f5c731c1ffd6324f75582f1399
vFf0621 <victorkwan1206@yahoo.com>
2021-10-16 04:05:05 -0400

    Updated session

--- src/demo2/Session.java
+++ src/demo2/Session.java
@@ -2,7 +2,10 @@ package demo2;
 
 public class Session {
     public static void main(String[] args) {
-
+        Database d = new Database();
+        Login l = new Login(d);
+        l.run();
+        l.run();
     }
 
 }

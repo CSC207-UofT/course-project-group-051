afa726d4115957b3c88a7762ae4689cbb8f1feff
vFf0621 <victorkwan1206@yahoo.com>
2021-10-16 03:10:03 -0400

    Update Login.java

--- src/demo2/Login.java
+++ src/demo2/Login.java
@@ -17,19 +17,20 @@ public class Login {
      * #TODO finalize this implementation. Should this have sout  and scan (as it is now) or will we have an input/output class?
      * this method prompts the user for information then creates a User for them.
      */
-    public void run(){
+    public void run() {
 
         String input;
 
         System.out.println("Would you like to Sign In or Create Account?");
         input = scanner.nextLine();
-        CurrentUser loginUser;
+        CurrentUser loginUser = null;
 
         //determine whether they want to sign in or create account.
+        String username;
+        String password;
         if (input == "Create Account") {
 
             CreateAccount newAcc = new CreateAccount(database.generateUniqueID());
-            String username, password;
 
             //Prompt for username and password.
             System.out.println("Username:");
@@ -44,11 +45,25 @@ public class Login {
             loginUser = new CurrentUser(newAcc.generateUser());
         } else {
             //#TODO create a login feature. Probably using information from a Database.
+
+            System.out.println("Username:");
+            username = scanner.nextLine();
+            System.out.println("Password");
+            password = scanner.nextLine();
+
+            for (Object u : database.getUsers()) {
+                if (u instanceof User) {
+                    if (((User) u).getPassword().equals(password)&& ((User) u).getUsername().equals(username)){
+                        loginUser = new CurrentUser((User) u);
+                    }
+                }
+            }
         }
 
         //Once the user is logged in, we pass them to the ProfileView.(this is red-underlined because the else hasn't been filled in)
+        if(loginUser instanceof CurrentUser){
         ProfileView profileView = new ProfileView(loginUser);
-        profileView.run();
+        profileView.run();}
 
     }
 

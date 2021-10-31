794c3c106a7688f21075f67945033ef9ae4546ad
vFf0621 <victorkwan1206@yahoo.com>
2021-10-30 21:54:06 -0400

    add ViewBuilder

---
+++ src/Phase1/Views/SceneBuilder.java
@@ -0,0 +1,20 @@
+package com.example.demoone;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+interface SceneBuilder {
+void setScene(Stage stage);
+void addTextField(TextField textfield, Label label);
+void addHBox(HBox hbox);
+void addVBox(VBox vbox);
+void setMargin(Insets inset);
+void setSpacing(HBox hbox, VBox vbox, int spacing);
+void mapEventHandler(Button button, EventHandler handler);
+}

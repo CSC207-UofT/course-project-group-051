0665221e516649b678c1c928a4a310338fb11d79
ShengHe Xiao <benny.xiao@mail.utoronto.ca>
2021-11-02 18:41:18 -0400

    X>(

--- src/Phase1/DataAccess/DataAccessInterface.java
+++ src/Phase1/DataAccess/DataAccessInterface.java
@@ -3,27 +3,65 @@ package Phase1.DataAccess;
 
 import Phase0.User;
 
+import java.util.ArrayList;
+
 /**
  * An interface that defines the methods that interact with the database.
  */
 public interface DataAccessInterface {
 
+    int logIn(String username, String password);
 
+    String getFirstName(int id);
 
-    User getProfileUser(int id);
+    String getLastName(int id);
 
-    User getMessageUser(int id);
+    String getUsername(int id);
 
-    User getSwipeUser(int id);
+    String getPassword(int id);
 
-    void UpdateUser(User u);
+    String getGender(int id);
 
-    void UpdateThread(Thread t);
+    String getBio(int id);
 
-    Thread getThread(int id);
+    String getGenderPreference(int id);
 
+    int getAge(int id);
 
+    ArrayList<Integer> getLikes(int id);
 
+    ArrayList<Integer> getAdmires(int id);
 
+    ArrayList<String> getThread(int threadID);
+
+    String getMessage(int messageID);
+
+    boolean setFirstName(int id, String firstName);
+
+    boolean setLastName(int id, String lastName);
+
+    boolean setUsername(int id, String username);
+
+    boolean setPassword(int id, String password);
+
+    boolean setGender(int id, String gender);
+
+    boolean setBio(int id, String bio);
+
+    boolean setAge(int id, int age);
+
+    boolean setGenderPreference(int id, String genderPreference);
+
+    boolean likeUser(int currUser, int likeID);
+
+    boolean unlikeUser(int currUser, int likeID);
+
+    boolean admireUser(int currUser, int admirerID);
+
+    boolean stopAdmiringUser(int currUser, int admirerID);
+
+    int createThread(int userID1, int userID2);
+
+    int createMessage(int threadID, int sender, int receiver);
 
 }
--- src/Phase1/DataAccess/DataBaseAccess.java
+++ src/Phase1/DataAccess/DataBaseAccess.java
@@ -8,33 +8,137 @@ public class DataBaseAccess implements DataAccessInterface{
 
 
     @Override
-    public User getProfileUser(int id) {
+    public int logIn(String username, String password) {
+        return 0;
+    }
+
+    @Override
+    public String getFirstName(int id) {
         return null;
     }
 
     @Override
-    public User getMessageUser(int id) {
+    public String getLastName(int id) {
         return null;
     }
 
     @Override
-    public User getSwipeUser(int id) {
+    public String getUsername(int id) {
         return null;
     }
 
     @Override
-    public void UpdateUser(User u) {
-
-    }
-
-    @Override
-    public void UpdateThread(Thread t) {
-
-    }
-
-
-    @Override
-    public Thread getThread(int id) {
+    public String getPassword(int id) {
         return null;
+    }
+
+    @Override
+    public String getGender(int id) {
+        return null;
+    }
+
+    @Override
+    public String getBio(int id) {
+        return null;
+    }
+
+    @Override
+    public String getGenderPreference(int id) {
+        return null;
+    }
+
+    @Override
+    public int getAge(int id) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Integer> getLikes(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> getAdmires(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> getThread(int threadID) {
+        return null;
+    }
+
+    @Override
+    public String getMessage(int messageID) {
+        return null;
+    }
+
+    @Override
+    public boolean setFirstName(int id, String firstName) {
+        return false;
+    }
+
+    @Override
+    public boolean setLastName(int id, String lastName) {
+        return false;
+    }
+
+    @Override
+    public boolean setUsername(int id, String username) {
+        return false;
+    }
+
+    @Override
+    public boolean setPassword(int id, String password) {
+        return false;
+    }
+
+    @Override
+    public boolean setGender(int id, String gender) {
+        return false;
+    }
+
+    @Override
+    public boolean setBio(int id, String bio) {
+        return false;
+    }
+
+    @Override
+    public boolean setAge(int id, int age) {
+        return false;
+    }
+
+    @Override
+    public boolean setGenderPreference(int id, String genderPreference) {
+        return false;
+    }
+
+    @Override
+    public boolean likeUser(int currUser, int likeID) {
+        return false;
+    }
+
+    @Override
+    public boolean unlikeUser(int currUser, int likeID) {
+        return false;
+    }
+
+    @Override
+    public boolean admireUser(int currUser, int admirerID) {
+        return false;
+    }
+
+    @Override
+    public boolean stopAdmiringUser(int currUser, int admirerID) {
+        return false;
+    }
+
+    @Override
+    public int createThread(int userID1, int userID2) {
+        return 0;
+    }
+
+    @Override
+    public int createMessage(int threadID, int sender, int receiver) {
+        return 0;
     }
 }

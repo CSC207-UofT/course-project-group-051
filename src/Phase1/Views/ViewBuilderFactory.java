24238ebdd5b1c092f99b5a004c81da01f476f631
vFf0621 <Victor-1206>
2021-11-02 17:58:10 -0700

    Added builder factory

--- src/Phase1/Views/ViewBuilderFactory.java
+++ src/Phase1/Views/ViewBuilderFactory.java
@@ -1,8 +1,32 @@
 package Phase1.Views;
 
+import Phase1.Users.ProfileUser;
+import Phase1.Users.User;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ViewBuilderFactory {
     public ViewBuilderFactory(){
+    }
 
+    public ChatViewBuilder cBuilders(ProfileUser u){
+            return new ChatViewBuilder(u.getfName());
+        }
 
+    public LogInViewBuilder lBuilder(){
+        return new LogInViewBuilder();
+    }
+
+    public RegistrationViewBuilder rBuilder(){
+        return new RegistrationViewBuilder();
+    }
+
+    public SelfViewBuilder sBuilder(ProfileUser u){
+        return new SelfViewBuilder(u);
+    }
+
+    public SwipeViewBuilder svBuilder(ImageView image, ProfileUser u){
+        return new SwipeViewBuilder(image, u);
     }
 }
+

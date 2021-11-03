c71b9b1efb69f56df14a1479e5855c9affb90091
vFf0621 <victorkwan1206@yahoo.com>
2021-11-03 01:12:05 -0400

    Update State.java

--- src/Phase1/States/State.java
+++ src/Phase1/States/State.java
@@ -5,7 +5,7 @@ import Phase1.UserActions.Action;
 /**
  * Represents a state of our program which determines what actions the user can take and what the user sees.
  */
-public abstract class State { //seems more like an interface.
+public abstract class State {
 
     public State(){}
 

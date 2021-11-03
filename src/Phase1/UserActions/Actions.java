4b0da09435b5477f2d03ec4a1e9e5dbfe12d8089
alexma22 <alex.mathioudakis@mail.utoronto.ca>
2021-11-02 21:53:21 -0400

    Changed the Actions.java to be a set of Constants instead.

---
+++ src/Phase1/UserActions/Actions.java
@@ -0,0 +1,22 @@
+package Phase1.UserActions;
+
+
+/**
+ * Imports the constants who refer to different user actions.
+ */
+public class Actions {
+
+
+    //#TODO delete the classes
+    public static final String BACK = "BACK";
+    public static final String LOGIN = "LOGIN";
+    public static final String LOGOUT = "LOGOUT";
+    public static final String MESSAGE = "MESSAGE";
+    public static final String REGISTER = "REGISTER";
+    public static final String SHOWMATCHES = "SHOWMATCHES";
+    public static final String SWIPELEFT = "SWIPELEFT";
+    public static final String SWIPERIGHT = "SWIPERIGHT";
+    public static final String VIEWSELF = "VIEWSELF";
+
+
+}

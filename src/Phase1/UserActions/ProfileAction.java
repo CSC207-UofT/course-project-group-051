7c9141c2f4a6c5cc294244519079614d1915ef94
senank <senankassem@gmail.com>
2021-11-06 21:10:11 -0400

    Skeleton for UserActions

---
+++ src/Phase1/UserActions/ProfileAction.java
@@ -0,0 +1,7 @@
+package Phase1.UserActions;
+
+public class ProfileAction {
+    public ProfileAction(int id){
+
+    }
+}
---
+++ src/Phase1/UserActions/SwipeAction.java
@@ -0,0 +1,11 @@
+package Phase1.UserActions;
+
+public class SwipeAction {
+    public int userID;
+    public SwipeAction(int id){
+        this.userID = id;
+    }
+    public void like() {
+
+    }
+}

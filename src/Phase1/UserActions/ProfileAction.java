fb9f1202df56801d1b8b4689d0816c72b9a119b6
senank <senankassem@gmail.com>
2021-11-06 21:20:59 -0400

    ProfileAction subclass updated

--- src/Phase1/UserActions/ProfileAction.java
+++ src/Phase1/UserActions/ProfileAction.java
@@ -1,7 +1,20 @@
 package Phase1.UserActions;
 
-public class ProfileAction {
+import Phase1.States.State;
+import javafx.scene.image.ImageView;
+
+public class ProfileAction extends UserActions{
+
+    private String bio;
+    private String gender;
+    private String preference;
+    private String username;
+    private String password;
+    private State state;
+    private ImageView image;
+
     public ProfileAction(int id){
+        super(id);
 
     }
 }

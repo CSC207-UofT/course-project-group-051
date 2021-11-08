3673b0c4c5429ff63aa5718ba8cf835c01fca73e
alexma22 <alex.mathioudakis@mail.utoronto.ca>
2021-11-08 00:30:41 -0500

    Mostly finished BuildProfileUser.java. Need to finalize User Attributes.

--- src/Phase1/Users/ProfileUser.java
+++ src/Phase1/Users/ProfileUser.java
@@ -1,24 +1,13 @@
 package Phase1.Users;
 
-import Phase1.States.LoggedOut;
 import Phase1.States.State;
 import javafx.scene.image.ImageView;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.temporal.Temporal;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Represents the User's information that pertains to editing/viewing profile information.
- *
- * @param bio is the bio inputted by the user
- * @param gender is the gender of the user
- * @param age is the age of the user
- * @param preference is the gender preference of the user
- * @param username is the username of the user
- * @param password is the password of the user to access their profile
  */
 public class ProfileUser extends User {
 
@@ -30,12 +19,10 @@ public class ProfileUser extends User {
     private State state;
     private ImageView image;
 
-    public ProfileUser(int id, String fName, String lName, Date birthdate, String password, ImageView image) {
-        super(id, fName, lName, birthdate, password);
-        this.state = new LoggedOut(); // FIXME: why is this logged out?
-        this.image = image;
-
+    public ProfileUser(int id) {
+        super(id);
     }
+
 
     public String getBio() {
         return this.bio;

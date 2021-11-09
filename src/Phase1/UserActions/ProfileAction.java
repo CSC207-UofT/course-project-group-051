5c59701dd703de42308bc59209c5c4442381cb0e
alexma22 <alex.mathioudakis@mail.utoronto.ca>
2021-11-09 04:14:44 -0500

    Added a semicolon

--- src/Phase1/UserActions/ProfileAction.java
+++ src/Phase1/UserActions/ProfileAction.java
@@ -1,21 +1,18 @@
 package Phase1.UserActions;
 
+import Phase1.UserBuilders.BuildProfileUser;
 import Phase1.Users.ProfileUser;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Map;
 
 public class ProfileAction implements Transitionable{
 
     private ProfileUser user;
 
-    public ProfileAction(int id, String fName, String lName, Date birthdate, String password, ImageView image){
-        this.user = new ProfileUser(id, db.getFirstName(id), db.getLastName(id),
-                db.getBirthday(), db.getPassword(), db.getImage());
+    public ProfileAction(int id) {
+        BuildProfileUser builder = new BuildProfileUser(id);
+        user = builder.Build();
 
     }
     private void updateImage(Image image){
@@ -36,7 +33,7 @@ public class ProfileAction implements Transitionable{
         db.setFirstName(this.user.getfName());
         db.setLastName(this.user.getlName());
         db.setBio(this.user.getBio());
-        db.setGender(this.user.getGender())
+        db.setGender(this.user.getGender());
         db.setGenderPreference(this.user.getPreference());
         db.setBirthday(this.user.getBirthdate());
         db.setPassword(this.user.getPassword());

37e901be441470bc8c78bff6dd2906a8a3d5de43
Ansonnnn <sanson.chen@mail.utoronto.ca>
2021-11-29 14:54:02 -0500

    Implement MatchController but need later refactoring once MessageView is done

--- src/phase2/controllers/MatchController.java
+++ src/phase2/controllers/MatchController.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import phase2.dataaccess.DataAccessInterface;
-
+import phase2.presenters.MessageView;
+import phase2.presenters.MatchView;
+import phase2.presenters.SwipeView;
 import java.util.ArrayList;
 
 public class MatchController {
@@ -27,15 +29,21 @@ public class MatchController {
 
         return null;
     }
-    public EventHandler<ActionEvent> switchMessageView(Integer userID){
+    public EventHandler<ActionEvent> switchMessageView(Integer userID, Integer receiverID){
         event = e -> {
-            MessageView(db, stage, userID, receiverID);
+            for (Integer id : getMatches()) {
+                if (id.equals(receiverID)) {
+                    MessageView m = new MessageView(db, stage, userID, receiverID);
+                    m.build();
+                }
+            }
         };
         return event;
     }
-    public EventHandler<ActionEvent> back(){
+    public EventHandler<ActionEvent> back() {
         event = e -> {
-            SwipeView(db, stage, userID, swipeList);
+            SwipeView s = new SwipeView(db, stage, userID, db.getSwipeList(userID));
+            s.build();
         };
         return event;
     }

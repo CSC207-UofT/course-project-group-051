fd9eb3572a1ba7ca6fcaa15e46c2e0c46bee1664
alexma22 <alex.mathioudakis@mail.utoronto.ca>
2021-12-05 21:38:21 -0500

    Finished the messanger classes.

--- src/phase2/controllers/MessageController.java
+++ src/phase2/controllers/MessageController.java
@@ -1,58 +1,100 @@
 package phase2.controllers;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import phase2.constants.State;
-import phase2.constants.States;
 import phase2.dataaccess.DataAccessInterface;
 import phase2.presenters.MatchView;
 import phase2.presenters.MessageView;
+import phase2.presenters.View;
 import phase2.usecase.MessageCase;
+
+import java.util.ArrayList;
 
 /** A controller that delegates the task for each button in the message view.
  */
 public class MessageController extends Controller{
-    DataAccessInterface db; // the data access interface.
-    Stage s; // the main stage.
-    int primaryUser; // the primary user ID.
-    int secondaryUser; // the secondary user ID
+
+
+    int currentUser; // the primary user ID.
+    int receiver; // the secondary user ID
     MessageCase messageCase;
 
     /** Creates an instance of MessageController.
      * @param db the data access interface.
      * @param stage the main stage.
-     * @param primary the id of primary user.
-     * @param secondary the id of secondary user.
+     * @param currentUser the id of currentUser user.
+     * @param receiver the id of receiver user.
      */
-    public MessageController(DataAccessInterface db, Stage stage, int primary, int secondary){
+    public MessageController(DataAccessInterface db, Stage stage, int currentUser, int receiver){
         super(db, stage);
-        this.primaryUser = primary;
-        this.secondaryUser = secondary;
-        State.setState(States.MESSAGING);
-        messageCase = new MessageCase(primary, secondary, db);
+        this.currentUser = currentUser;
+        this.receiver = receiver;
+        messageCase = new MessageCase(db, currentUser, receiver);
     }
 
-    /**
-     * @return the event handler when the "Send" button is pressed.
-     */
 
-    public EventHandler<ActionEvent> send(String msg){
+    /**
+     * @return An EventHandler that sends a message to the receiver.
+     */
+    public EventHandler<ActionEvent> send(TextField msg){
         return event -> {
-            messageCase.CreateMessage(msg);
-            MessageView m = new MessageView(db, s, primaryUser, secondaryUser);
-            m.build();
+
+            messageCase.sendMessage(msg.getText());
+            View view = new MessageView(receiver);
+            view.build();
+
         };
 
     }
+
+
     /**
-     * @return the event handler when the "Back" button is pressed.
+     * @return An EventHandler that returns the currentUser to the Matches screen.
      */
     public EventHandler<ActionEvent> back(){
 
         return event -> {
-            MatchView mb = new MatchView(db, s, primaryUser);
-            mb.build();
+            View view = new MatchView();
+            view.build();
         };
     }
 
+
+    /**
+     * @return A String representation of the receiver of the messages.
+     */
+    public String getReciever() {
+
+        return messageCase.getReceiverName();
+
+    }
+
+
+    /**
+     * @return the messages of this thread as an ArrayList of arrays of Strings. Where each Array is a message
+     * in the following format: {MessageContent, Sender, Receiver}.
+     */
+    public ArrayList<String[]> getThread(){
+        return messageCase.getFullThread();
+    }
+
+
+    /**
+     * Determines whether the currentUser is the sender or receiver of this message, which will determine if the
+     * message displays on the left or right side of the screen.
+     * @param sender the sender of this message.
+     * @return The side of the messageView that the message should go as a String.
+     */
+    public String determineSender(String sender) {
+
+        int senderID = Integer.parseInt(sender);
+
+        if (senderID == currentUser) {
+            return "-fx-background-color: lightblue; -fx-background-radius: 10;";
+        }
+
+        return "-fx-background-color: palegreen; -fx-background-radius: 10;";
+
+    }
 }

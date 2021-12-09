77796229d14f3bd006a063a90df071bbf323d1f2
A-Greenfoot-Bug <53586218+A-Greenfoot-Bug@users.noreply.github.com>
2021-12-08 20:12:28 -0500

    changed db setup

--- src/phase2/dataaccess/DataBaseAccess.java
+++ src/phase2/dataaccess/DataBaseAccess.java
@@ -813,9 +813,9 @@ public class DataBaseAccess implements DataAccessInterface {
 
     public void setUpDB(){
         try {
-            String h2 = "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (1, 'person1L', 'person1F', 'user1', 'password', 1, 'Male', 'Female', 'hi', '2', '2', '', 'Dec,01,2021', '.\\img\\img1.jpg');\n" +
-                    "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (2, 'person2L', 'person2F', 'user2', 'password', 1, 'Female', 'Male', 'hi', '1', '1', '', 'Dec,01,2021', '.\\img\\img2.jpg');\n" +
-                    "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (3, 'person3L', 'person3F', 'user3', 'password', 1, 'Female', 'Male', 'hi', '', '', '', 'Dec,01,2021', '.\\img\\img3.jpg');";
+            String h2 = "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (1, 'person1L', 'person1F', 'user1', 'password', 19, 'Male', 'Female', 'hi', '', '', '', 'Dec,01,2021', '.\\img\\img1.jpg');\n" +
+                    "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (2, 'person2L', 'person2F', 'user2', 'password', 19, 'Female', 'Male', 'hi', '', '', '', 'Dec,01,2021', '.\\img\\img2.jpg');\n" +
+                    "INSERT INTO PUBLIC.USER (PERSONID, LASTNAME, FIRSTNAME, USERNAME, PASSWORD, AGE, GENDER, GENDERPREFERENCE, BIO, LIKES, ADMIRES, THREADS, BIRTHDAY, IMGLOCATION) VALUES (3, 'person3L', 'person3F', 'user3', 'password', 19, 'Female', 'Male', 'hi', '', '', '', 'Dec,01,2021', '.\\img\\img3.jpg');";
             stmt.execute(h2);
 
         } catch (SQLException se) {
